baaa3b7423edbc79efad55ae59ea38996ec77efb
HitomiHuang <black011602@gmail.com>
2023-07-13 22:01:57 +0800

    install dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    
-  </div>
+  <div></div>
 </template>
 
 <script>
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 
