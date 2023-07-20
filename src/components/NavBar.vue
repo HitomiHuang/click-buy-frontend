050345934679f4110db0d409c963fbf41bee4c97
HitomiHuang <black011602@gmail.com>
2023-07-20 23:21:31 +0800

    add header

---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav>
+    <div class="nav-items left">
+      <a href="#">賣家中心</a>
+      <div class="line"></div>
+      <a href="#">追蹤我們</a>
+    </div>
+    <div class="nav-items right">
+      <a href="#">幫助中心</a>
+      <a href="#" class="nav-item">註冊</a>
+      <div class="line"></div>
+      <a href="#">登入</a>
+      <!-- <a href="#" class="nav-item">
+        <img src="#" alt="">
+        <span>userName</span>
+      </a> -->
+    </div>
+  </nav>
+</template>
+<script>
+</script>
+<style>
+nav {
+  height: 34px;
+  background: transparent;
+  display: flex;
+  justify-content: space-between;
+}
+a {
+  line-height: 34px;
+  text-decoration: none;
+  color: white;
+}
+ul {
+  list-style: none;
+  display: flex;
+}
+.nav-items {
+  display:flex;
+  align-items: center;
+
+}
+.nav-item{
+  margin-left:12px;
+}
+.line {
+  width: 2px;
+  height: 15px;
+  background: #F86C5C;
+  margin: 0px 10px;
+  display: inline-block;
+}
+.left{
+  width: 30%;
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="search-block">
+    <div class="logo"></div>
+    <div class="search-bar">
+      <input type="text" class="search-input" placeholder="請輸入關鍵字">
+      <button class=" btn btn-primary search-btn"><i class="fa-solid fa-magnifying-glass d-block"></i></button>
+    </div>
+    <div class="cart">
+      <a href="#"><i class="fa-solid fa-cart-shopping fa-xl d-block"></i></a>    
+    </div>
+  </div>
+</template>
+<script>
+</script>
+<style>
+.search-block{
+  height: 85px;
+  background: transparent;
+  display: flex;
+  align-items: center;
+}
+.logo{
+  flex:2;
+  background-image: url('../assets/click-buy-logo-3.png');
+  width: 162px;
+  height: 50px;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+.search-bar{
+  flex:8;
+  position: relative;
+}
+.search-input{
+  width:100%;
+  padding: 3px 73px 3px 10px;
+  height: 40px;
+  border: none;
+  border-radius: 3px;
+}
+.search-btn{
+  width: 60px;
+  height: 34px;
+  position: absolute;
+  right:3px;
+  top: 3px;
+  border-radius: 3px;
+  padding-bottom: 5px;
+  background: #FB5533;
+}
+.cart{
+  flex:1;
+  display: flex;
+  justify-content: center;
+  color:white;
+  cursor: pointer;
+}
+
+</style>
